ea49545bc63d6d23a3dd1240c05b5148dd88c124
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-07-10 11:16:28 +0530

    day 2

--- app/src/main/java/com/abhitom/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/abhitom/whatsappclone/MainActivity.kt
@@ -1,27 +1,61 @@
 package com.abhitom.whatsappclone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.google.android.gms.auth.api.credentials.*
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var phoneNumber:String
     private lateinit var countryCode:String
     private lateinit var alertDialogBox:MaterialAlertDialogBuilder
+    val PHONE_REQUEST=101
+    var phoneNumberHint:String?=""
+    val PHONE_NUMBER="PhoneNumber"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etPhoneNumber.addTextChangedListener {
             btnNext.isEnabled=!(it.isNullOrEmpty() || it.length<10)
         }
+        val hintRequest = HintRequest.Builder()
+            .setPhoneNumberIdentifierSupported(true)
+            .setEmailAddressIdentifierSupported(false)
+            .build()
+        val options = CredentialsOptions.Builder()
+            .forceEnableSaveDialog()
+            .build()
+        val pendingIntent = Credentials.getClient(this, options).getHintPickerIntent(hintRequest)
+        startIntentSenderForResult(pendingIntent.intentSender, PHONE_REQUEST, null, 0, 0, 0)
+
+
         btnNext.setOnClickListener {
             countryCode=countryCodeHolder.selectedCountryCodeWithPlus
             phoneNumber=countryCode+etPhoneNumber.text.toString()
             showDialogBox()
         }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PHONE_REQUEST) {
+            if (data != null) {
+                val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
+                phoneNumberHint = credential?.id
+                showPhoneNumberHint()
+                // Some of our device fail to get phoneNumber here.
+            }
+        }
+    }
+
+    private fun showPhoneNumberHint() {
+        val len=phoneNumberHint?.length
+        phoneNumberHint=phoneNumberHint?.substring(3,len!!)
+        etPhoneNumber.setText(phoneNumberHint)
     }
 
     private fun showDialogBox() {
@@ -40,7 +74,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showLoginActivity() {
-        startActivity(Intent(this, OTPActivity::class.java).putExtra("PhoneNumber", phoneNumber))
+        startActivity(Intent(this, OTPActivity::class.java).putExtra(PHONE_NUMBER, phoneNumber))
         finish()
     }
 }
--- app/src/main/java/com/abhitom/whatsappclone/OTPActivity.kt
+++ app/src/main/java/com/abhitom/whatsappclone/OTPActivity.kt
@@ -2,10 +2,16 @@ package com.abhitom.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_o_t_p.*
 
 class OTPActivity : AppCompatActivity() {
+    val PHONE_NUMBER="PhoneNumber"
+    private lateinit var phoneNumber:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_o_t_p)
+        phoneNumber=intent.getStringExtra(PHONE_NUMBER)
+        tvVerifyNumber.text=getString(R.string.verify_number,phoneNumber)
+        tvVerifyInfo.text="waiting for automatically detect an SMS sent to $phoneNumber"
     }
 }
