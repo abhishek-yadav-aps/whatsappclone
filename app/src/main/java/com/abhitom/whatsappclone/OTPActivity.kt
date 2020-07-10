92a25745419da3a48104eb6c50282241e55129a9
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-07-10 11:34:34 +0530

    day 3

--- app/src/main/java/com/abhitom/whatsappclone/OTPActivity.kt
+++ app/src/main/java/com/abhitom/whatsappclone/OTPActivity.kt
@@ -2,7 +2,14 @@ package com.abhitom.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import kotlinx.android.synthetic.main.activity_o_t_p.*
+import java.util.*
 
 class OTPActivity : AppCompatActivity() {
     val PHONE_NUMBER="PhoneNumber"
@@ -12,6 +19,24 @@ class OTPActivity : AppCompatActivity() {
         setContentView(R.layout.activity_o_t_p)
         phoneNumber=intent.getStringExtra(PHONE_NUMBER)
         tvVerifyNumber.text=getString(R.string.verify_number,phoneNumber)
-        tvVerifyInfo.text="waiting for automatically detect an SMS sent to $phoneNumber"
+        setSpannableString()
+    }
+
+    private fun setSpannableString() {
+        val span =SpannableString(getString(R.string.waiting_text,phoneNumber))
+        val clickableSpan= object :ClickableSpan(){
+            override fun onClick(widget: View) {
+
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText=false
+                ds.color=ds.linkColor
+            }
+        }
+        span.setSpan(clickableSpan,span.length-13,span.length,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tvVerifyInfo.text=span
+        tvVerifyInfo.movementMethod=LinkMovementMethod.getInstance()
     }
 }
