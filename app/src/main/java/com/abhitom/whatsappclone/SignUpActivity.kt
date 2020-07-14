85c0b20e51e58dc30f7e8a715e8662b6519290c9
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-07-14 13:56:58 +0530

    day 8

--- app/src/main/java/com/abhitom/whatsappclone/SignUpActivity.kt
+++ app/src/main/java/com/abhitom/whatsappclone/SignUpActivity.kt
@@ -4,13 +4,23 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.RequiresApi
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+    val mStorage =FirebaseStorage.getInstance()
+    val mAuth=FirebaseAuth.getInstance()
+    var downloadUrl=""
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,8 +53,29 @@ class SignUpActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode== Activity.RESULT_OK && requestCode==1000){
-            data?.data.let {
+            data?.data?.let {
                 sivProfilePic.setImageURI(it)
+                uploadImage(it)
+            }
+        }
+    }
+
+    private fun uploadImage(it: Uri) {
+        btnNext.isEnabled=false
+        val mRef=mStorage.reference.child("ProfilePhotoUploads/"+mAuth.uid.toString())
+        val uploadTask=mRef.putFile(it)
+        uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot,Task<Uri>>{ task ->
+            if (!task.isSuccessful){
+                task.exception?.let {
+                    throw it
+                }
+            }
+            return@Continuation mRef.downloadUrl
+        }).addOnCompleteListener { task->
+            btnNext.isEnabled=true
+            if(task.isSuccessful){
+                downloadUrl=task.result.toString()
+                Log.i("URL","downloadUrl : $downloadUrl")
             }
         }
     }
---
+++ app/src/main/java/com/abhitom/whatsappclone/User.kt
@@ -0,0 +1,14 @@
+package com.abhitom.whatsappclone
+
+class User(
+    val name:String,
+    val imageUrl:String,
+    val thumbImage:String,
+    val uid:String,
+    val deviceToken:String,
+    val status:String,
+    val onlineStatus: Long
+) {
+    constructor():this("","","","","","",System.currentTimeMillis())
+    constructor(name: String,imageUrl: String,thumbImage: String,uid: String):this(name,imageUrl,thumbImage,uid,"","Hey there!! I am using whatsapp",System.currentTimeMillis())
+}
